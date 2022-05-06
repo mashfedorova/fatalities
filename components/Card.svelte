df5de9076a77834fb6eb598329b9927a517414d8
FEDOROVA Maria <marie.fedorova@gmail.com>
2022-05-05 18:46:19 -0600

    carousel

---
+++ components/Card.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let story
+</script>
+
+<!-- <div> -->
+<p class="container">{story}</p>
+
+<!-- </div> -->
+<style>
+  /* div {
+    padding: 50px;
+  } */
+  .container {
+    height: 50px;
+    padding-top: 25px;
+    color: rgba(46, 46, 46, 0.878);
+    /* width: 200px; */
+    /* border: 1px solid black;
+    background: white;
+    padding: 50px; */
+    /* transform-style: preserve-3D; */
+    /* border: 1px solid black; */
+    /* background-color: rgb(221, 221, 221, 0.1); */
+    /* box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px; */
+  }
+</style>
--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,8 @@
   import ForceAverages from "../components/ForceAverages.svelte"
   import Monthly from "../components/Monthly.svelte"
   import States from "../components/States.svelte"
+  import Card from "../components/Card.svelte"
+  import Carousel from "svelte-carousel"
 
   let width = document.body.clientWidth
   let circles = []
@@ -79,6 +81,13 @@
   })
 
   // $: console.log(circles)
+
+  let stories = [
+    "Worker died in fall from roof",
+    "Worker fatally struck by pickup truck",
+    "Worker fatally injured while training horse",
+    "Worker fatally crushed when mower rolled over.",
+  ]
 </script>
 
 <svelte:window on:resize={resize} />
@@ -107,8 +116,36 @@
       <text y={height - 60} x="360">largerly due to COVID-19</text>
     </svg>
   </div>
-  <div class="monthly">
+  <!-- <div class="monthly">
     <Monthly {monthly} {blue} />
+  </div> -->
+  <div class="carousel-container">
+    <div class="carousel">
+      <Carousel
+        let:showPrevPage
+        let:showNextPage
+        autoplay
+        autoplayDuration={2000}
+      >
+        <div
+          slot="prev"
+          on:click={showPrevPage}
+          class="custom-arrow custom-arrow-prev"
+        >
+          <i class="arrow arrow-left" />
+        </div>
+        {#each stories as story}
+          <Card {story} />
+        {/each}
+        <div
+          slot="next"
+          on:click={showNextPage}
+          class="custom-arrow custom-arrow-next"
+        >
+          <i class="arrow arrow-right" />
+        </div>
+      </Carousel>
+    </div>
   </div>
   <div class="monthly">
     <States {states} {statesSorted} {blue} />
@@ -130,5 +167,49 @@
     padding: 1em;
     max-width: 1200px;
     margin: 0 auto;
+  }
+
+  .carousel-container {
+    display: flex;
+    justify-content: center;
+  }
+
+  .carousel {
+    width: 500px;
+    border: 1px solid rgba(88, 88, 88, 0.81);
+    padding: 20px 0;
+    margin-bottom: 20px;
+    /* box-shadow: 20px 20px rgba(0, 0, 0, 0.15); */
+    box-shadow: rgba(117, 117, 117, 0.15) 2.5px 2.5px 3.2px;
+    /* box-shadow: rgba(0, 0, 0, 0.15) 2.6px 2.6px 3.5px; */
+  }
+
+  .custom-arrow {
+    margin: auto 10px;
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background-color: white;
+    border: 1px solid grey;
+    transition: opacity 100ms ease;
+    cursor: pointer;
+    box-shadow: rgba(121, 121, 121, 0.15) 1px 1px 1px;
+  }
+
+  .arrow {
+    border: solid rgba(153, 153, 153, 0.858);
+    border-width: 0 2px 2px 0;
+    display: inline-block;
+    padding: 2px;
+  }
+
+  .arrow-left {
+    transform: rotate(135deg);
+    -webkit-transform: rotate(135deg);
+  }
+
+  .arrow-right {
+    transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
   }
 </style>
